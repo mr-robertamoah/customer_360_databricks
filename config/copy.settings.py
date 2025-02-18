152dd990ea69493c333e4b034de6fff1899d3d35
mr_robertamoah@yahoo.com <mr_robertamoah@yahoo.com>
2025-02-18 07:43:51 +0000

    set up structure of project and added necessary variables needed for ingesting from RDS, Kinesis and S3

---
+++ config/copy.settings.py
@@ -0,0 +1,25 @@
+# rename file to settings.py and enter required fields
+
+S3_BUCKET_NAME = ""
+S3_BUCKET_URI = f"s3://{S3_BUCKET_NAME}"
+
+TRANSACTIONS_PATH = f"{S3_BUCKET_URI}/transactions/"
+ORDERS_PATH = f"{S3_BUCKET_URI}/orders/"
+FACEBOOK_PATH = f"{S3_BUCKET_URI}/facebook/"
+PREFERENCES_PATH = f"{S3_BUCKET_URI}/preferences/"
+
+RDS_HOST = ""
+RDS_USER = ""
+RDS_PASSWORD = ""
+RDS_DATABASE = ""
+RDS_PORT = "3306"
+
+# Kinesis Stream Details
+KINESIS_STREAM = ""
+
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_KEY = ""
+AWS_REGION = ""
+
+# Other global settings
+DATA_INGESTION_BATCH_SIZE = 500
